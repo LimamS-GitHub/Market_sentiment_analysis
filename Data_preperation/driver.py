2e50c1016de2c35d33b27871631313db83c96fc5
LimamS-Github <selimlimam2001@gmail.com>
2025-03-16 22:17:34 +0100

    changes_driver_file

--- Data_preperation/driver.py
+++ Data_preperation/driver.py
@@ -16,6 +16,7 @@ def initialize_driver():
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/58.0.3029.110 Safari/537.3"
     )
+    
     return webdriver.Chrome(service=service, options=options)
 
 #------------------------------------------------------------------------------------------------------------------
